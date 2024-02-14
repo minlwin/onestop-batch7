aef1a7f992dc1a6d9ec8e303b5b7493aa6705fca
Min Lwin <lwin.zawmin@gmail.com>
2024-02-14 20:15:00 +0630

    Adding Auditing Feature

--- payroll-api/payroll-api-application/src/main/java/com/jdc/payroll/PayrollApplication.java
+++ payroll-api/payroll-api-application/src/main/java/com/jdc/payroll/PayrollApplication.java
@@ -2,8 +2,10 @@ package com.jdc.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PayrollApplication {
 
 	public static void main(String[] args) {
