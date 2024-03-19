a372bcf55e3c00daafdf9258c2b31c7284dc4698
Min Lwin <lwin.zawmin@gmail.com>
2024-03-19 19:09:24 +0630

    Update Token Provider

--- payroll-api/payroll-api-application/src/main/java/com/jdc/payroll/configuration/ApiSecurityConfiguration.java
+++ payroll-api/payroll-api-application/src/main/java/com/jdc/payroll/configuration/ApiSecurityConfiguration.java
@@ -2,6 +2,9 @@ package com.jdc.payroll.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +15,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.jdc.payroll.security.service.ApiTokenFilter;
 
 @Configuration
+@PropertySource({
+	"/app-token.properties"
+})
 public class ApiSecurityConfiguration {
 
 	@Bean
@@ -33,6 +39,11 @@ public class ApiSecurityConfiguration {
 		http.addFilterBefore(apiTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 		
 		return http.build();
+	}
+	
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
 	}
 	
 	@Bean
--- payroll-api/payroll-api-application/src/main/java/com/jdc/payroll/security/service/ApiTokenProvider.java
+++ payroll-api/payroll-api-application/src/main/java/com/jdc/payroll/security/service/ApiTokenProvider.java
@@ -1,17 +1,46 @@
 package com.jdc.payroll.security.service;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Jwts;
+
 @Service
 public class ApiTokenProvider {
+	
+	private static final String ROLE = "rol";
+	
+	@Value("${app.token.issuer}")
+	private String issuer;
+	@Value("${app.token.life}")
+	private int lifeTime;
+	@Value("${app.token.secret}")
+	private String secret;
+	
+	public String generate(Authentication authentication) {
+		
+		var issueAt = new Date();
+		var calendar = Calendar.getInstance();
+		calendar.setTime(issueAt);
+		calendar.add(Calendar.MINUTE, lifeTime);
+		
+		return Jwts.builder()
+			.subject(authentication.getName())
+			.issuer(issuer)
+			.issuedAt(issueAt)
+			.expiration(calendar.getTime())
+			.compact();		
+	}
 
 	public Authentication parse(String token) {
+		
+		
+		
 		return null;
 	}
 
-	public String generate(Authentication authentication) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
